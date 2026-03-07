d57e678737606442a95de71ec9d094b487fb6e22
Brian <bkimmle@gmail.com>
2026-03-07 09:21:29 -0600

    Updated to libcron, more reliable time keeping

--- src/scheduler/cron-scheduler.cpp
+++ src/scheduler/cron-scheduler.cpp
@@ -3,34 +3,23 @@
 #include "warp/log/log.h"
 #include "warp/log/log-utils.h"
 
-#include <croncpp.h>
+#include <libcron/Cron.h>
 
 #include <algorithm>
 #include <ranges>
 
 namespace warp
 {
-   // Internal structure only visible in this .cpp
-   struct CronTask
-   {
-      Task task;
-      cron::cronexpr cron;
-      std::chrono::system_clock::time_point nextRun;
-   };
-
    class CronScheduler::CronSchedulerImpl
    {
    public:
-      std::vector<CronTask> tasks_;
+      libcron::Cron<libcron::LocalClock, libcron::Locker> cronTasks_;
       std::mutex mtx_;
       std::condition_variable_any cv_;
       std::unique_ptr<std::jthread> runThread_;
 
       void Add(const Task& task);
 
-      std::chrono::system_clock::time_point GetNextRunTime() const;
-
-      void RunTasks();
       void Work(std::stop_token stopToken);
       bool Start();
       void Shutdown();
@@ -54,26 +43,21 @@ namespace warp
          return;
       }
 
-      try
-      {
-         // Construct the cron first as this will throw an exception if invalid.
-         // This prevents a task being added with invalid cron
-         auto cron = cron::make_cron(task.cronExpression);
+      log::Info("{}: Enabled - Schedule: {}", task.name, task.cronExpression);
 
-         auto& cronTask = tasks_.emplace_back();
-         cronTask.task = task;
-         cronTask.cron = cron;
-         cronTask.nextRun = cron::cron_next(cronTask.cron, std::chrono::system_clock::now());
-
-         log::Trace("Cron Scheduler: Added task {} with {}",
-                    GetTag("name", cronTask.task.name),
-                    GetTag("cron", cronTask.task.cronExpression));
-      }
-      catch (const cron::bad_cronexpr& ex)
-      {
-         log::Error("Cron Scheduler: {} bad CRON {} - {}",
-                    task.name, task.cronExpression, ex.what());
-      }
+      bool success = cronTasks_.add_schedule(task.name, task.cronExpression, [task](auto& info) {
+         try
+         {
+            log::Trace("Cron Scheduler: Running task {} with {}",
+                       GetTag("name", task.name),
+                       GetTag("cron", task.cronExpression));
+            task.func();
+         }
+         catch (const std::exception& e)
+         {
+            // Log using your warp logger
+         }
+      });
    }
 
    void CronScheduler::Add(const Task& task)
@@ -81,58 +65,19 @@ namespace warp
       pimpl_->Add(task);
    }
 
-   std::chrono::system_clock::time_point CronScheduler::CronSchedulerImpl::GetNextRunTime() const
-   {
-      if (tasks_.empty()) return std::chrono::system_clock::time_point::max();
-
-      // Use std::min_element to find the earliest task in one line
-      auto it = std::ranges::min_element(tasks_,
-          [](const auto& a, const auto& b) { return a.nextRun < b.nextRun; });
-
-      return it->nextRun;
-   }
-
-   void CronScheduler::CronSchedulerImpl::RunTasks()
-   {
-      auto currentTime = std::chrono::system_clock::now();
-      for (auto& task : tasks_)
-      {
-         // Should this task be run
-         if (task.nextRun <= currentTime)
-         {
-            log::Trace("Cron Scheduler: Running task {} with {}",
-                       GetTag("name", task.task.name),
-                       GetTag("cron", task.task.cronExpression));
-            try
-            {
-               task.task.func();
-            }
-            catch (const std::exception& e)
-            {
-               log::Error("Task {} failed: {}", task.task.name, e.what());
-            }
-
-            // Update nextRun for next time
-            task.nextRun = cron::cron_next(task.cron, currentTime);
-         }
-      }
-   }
-
    void CronScheduler::CronSchedulerImpl::Work(std::stop_token stopToken)
    {
       while (!stopToken.stop_requested())
       {
-         // Get the next time we need to wake up
-         auto nextWaitPoint = GetNextRunTime();
+         cronTasks_.tick();
 
-         // Wait until the next scheduled task or a stop is requested
-         std::unique_lock<std::mutex> lock(mtx_);
-         cv_.wait_until(lock, stopToken, nextWaitPoint, [&] { return false; });
+         std::unique_lock lock(mtx_);
+         cv_.wait_for(lock, stopToken, std::chrono::milliseconds(1000), [&] {
+            return stopToken.stop_requested();
+         });
 
          // If a stop is requested break out of the loop
          if (stopToken.stop_requested()) break;
-
-         RunTasks();
       }
 
       log::Info("Cron Scheduler: Work thread shutting down");
@@ -140,19 +85,11 @@ namespace warp
 
    bool CronScheduler::CronSchedulerImpl::Start()
    {
-      if (tasks_.empty()) return false;
+      if (cronTasks_.count() == 0) return false;
 
       // jthread starts immediately and manages its own lifetime
       runThread_ = std::make_unique<std::jthread>([this](std::stop_token st) { Work(st); });
 
-      for (const auto& cronTask : tasks_)
-      {
-         if (cronTask.task.service)
-         {
-            log::Info("{}: Enabled - Schedule: {}",
-                      cronTask.task.name, cronTask.task.cronExpression);
-         }
-      }
       return true;
    }
 
