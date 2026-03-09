59be74eff0390aaab3bb81e06d597e9137c254f5
Brian <bkimmle@gmail.com>
2026-03-08 19:31:38 -0500

    update dependencies

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,7 +1,7 @@
 # Version Definitions
 set(SPDLOG_VERSION  v1.17.0)
-set(GLAZE_VERSION   v7.0.2)
-set(HTTPLIB_VERSION v0.34.0)
+set(GLAZE_VERSION   v7.1.1)
+set(HTTPLIB_VERSION v0.37.0)
 set(LIBCRON_VERSION v1.3.3)
 set(PUGIXML_VERSION v1.15)
 
