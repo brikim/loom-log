816918702282c84cad7c055f68c4fcefc1ad3456
Brian <bkimmle@gmail.com>
2026-08-08 08:36:35 -0500

    updated dependencies

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,9 +1,9 @@
 # Version Definitions
 set(SPDLOG_VERSION  v1.17.0)
-set(GLAZE_VERSION   v7.3.3)
-set(HTTPLIB_VERSION v0.42.0)
+set(GLAZE_VERSION   v8.0.0)
+set(HTTPLIB_VERSION v0.52.0)
 set(LIBCRON_VERSION v1.3.3)
-set(PUGIXML_VERSION v1.15)
+set(PUGIXML_VERSION v1.16)
 
 # Repository URLs (optional, but good for one-spot editing)
 set(SPDLOG_REPO  https://github.com/gabime/spdlog.git)
